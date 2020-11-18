db8e53708227406a71588aaa276d0bc733b265c1
Rémi Bruyère <remi.bruyere@affluences.com>
2020-11-18 11:34:42 +0100

    WIP: add motorisation to body

--- object/human_body.py
+++ object/human_body.py
@@ -7,6 +7,7 @@ class HumanBody(object):
     """Class to create a robot"""
 
     body = None
+    DEFAULT_MOTOR_FORCE = 100
 
     def __init__(self, client: ClientPyBullet, base_position=None, base_orientation=None, scaling=1.0):
         if client is None:
@@ -52,15 +53,15 @@ class HumanBody(object):
                 self.join_dict_revolute[join_name] = j
                 lower, upper = (info[8], info[9])
                 self.ordered_joints.append((j, lower, upper))
-                self.client.setJointMotorControl2(self.body, j, controlMode=self.client.VELOCITY_CONTROL, force=0)
+                self.client.setJointMotorControl2(self.body, j, controlMode=self.client.TORQUE_CONTROL,
+                                                  force=self.DEFAULT_MOTOR_FORCE)
             elif info[2] == self.client.JOINT_SPHERICAL:
                 join_name = info[1].decode("ascii")
                 self.join_dict_spherical[join_name] = j
                 lower, upper = (info[8], info[9])
                 self.ordered_joints.append((j, lower, upper))
-                self.client.setJointMotorControlMultiDof(self.body, j, controlMode=self.client.POSITION_CONTROL,
-                                                         targetPosition=[0, 0, 0], targetVelocity=[0, 0, 0],
-                                                         force=[100])
+                self.client.setJointMotorControlMultiDof(self.body, j, controlMode=self.client.TORQUE_CONTROL,
+                                                         force=[self.DEFAULT_MOTOR_FORCE])
 
     def initialise_motor_power(self):
         self.motor_name_revolute += ["right_knee", "left_knee"]
@@ -69,9 +70,121 @@ class HumanBody(object):
         self.motor_power_revolute += [75, 75]
         self.motors_revolute = [self.join_dict_revolute[n] for n in self.motor_name_revolute]
 
+        self.motor_name_spherical += ["chest"]
+        self.motor_power_spherical += [10]
+        self.motor_name_spherical += ["neck"]
+        self.motor_power_spherical += [10]
+        self.motor_name_spherical += ["right_hip", "left_hip"]
+        self.motor_power_spherical += [10, 10]
+        self.motor_name_spherical += ["right_ankle", "left_ankle"]
+        self.motor_power_spherical += [10, 10]
         self.motor_name_spherical += ["right_shoulder", "left_shoulder"]
         self.motor_power_spherical += [10, 10]
         self.motors_spherical = [self.join_dict_spherical[n] for n in self.motor_name_spherical]
+
+    # Revolute join
+
+    def get_force_right_knee(self):
+        limit = 15
+        ac = np.clip(10, -limit, limit)
+        return self.motor_power_revolute[0] * ac * 0.082
+
+    def get_force_left_knee(self):
+        limit = 15
+        ac = np.clip(10, -limit, limit)
+        return self.motor_power_revolute[1] * ac * 0.082
+
+    def get_force_right_elbow(self):
+        limit = 15
+        ac = np.clip(10, -limit, limit)
+        return self.motor_power_revolute[2] * ac * 0.082
+
+    def get_force_left_elbow(self):
+        limit = 15
+        ac = np.clip(10, -limit, limit)
+        return self.motor_power_revolute[3] * ac * 0.082
+
+    # Spherical join
+
+    def get_new_position_chest(self, position_to_add: (float, float, float)):
+        """Move chest
+        - position_to_add = (tilt chest to the sides, chest rotation, chest forward / backward)
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 1)[0]
+        return 1, (join_state[0] + position_to_add[0],  # Inclinaison sur les côtés
+                   join_state[1] + position_to_add[1],  # Rotation du torse
+                   join_state[2] + position_to_add[2],  # Torse vers l'avant / arrière
+                   join_state[3])  # Un poids ?
+
+    def get_new_position_neck(self, position_to_add: float):
+        """Move neck
+        - position_to_add = neck forward / backward
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 2)[0]
+        return 2, (join_state[0],  # Inclinaison du cou sur les côtés -> inutile
+                   join_state[1],  # Rotation de la tête -> inutile
+                   join_state[2] + position_to_add,  # Cou vers l'avant / arrière
+                   join_state[3])  # Un poids ?
+
+    def get_new_position_hip_right(self, position_to_add: (float, float)):
+        """Move hip
+        - position_to_add = (tilt leg to the sides, leg forward / backward)
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 3)[0]
+        return 3, (join_state[0] + position_to_add[0],  # Jambe vers l'exterieur
+                   join_state[1],  # Rotation de l'axe de la jambe -> inutile
+                   join_state[2] + position_to_add[1],  # Jambe vers l'avant / arrière
+                   join_state[3])  # Un poids ?
+
+    def get_new_position_hip_left(self, position_to_add: (float, float)):
+        """Move hip
+        - position_to_add = (tilt leg to the sides, leg forward / backward)
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 9)[0]
+        return 9, (join_state[0] + position_to_add[0],  # Jambe vers l'exterieur
+                   join_state[1],  # Rotation de l'axe de la jambe -> inutile
+                   join_state[2] + position_to_add[1],  # Jambe vers l'avant / arrière
+                   join_state[3])  # Un poids ?
+
+    def get_new_position_ankle_right(self, position_to_add: float):
+        """Move ankle
+        - position_to_add = ankle forward / backward
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 5)[0]
+        return 5, (join_state[0],  # Pied vers l'exterieur -> inutile
+                   join_state[1],  # Rotation du pied -> inutile
+                   join_state[2] + position_to_add,  # Pied vers l'avant / arrière
+                   join_state[3])  # Un poids ?
+
+    def get_new_position_ankle_left(self, position_to_add: float):
+        """Move ankle
+        - position_to_add = ankle forward / backward
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 11)[0]
+        return 11, (join_state[0],  # Pied vers l'exterieur -> inutile
+                    join_state[1],  # Rotation du pied -> inutile
+                    join_state[2] + position_to_add,  # Pied vers l'avant / arrière
+                    join_state[3])  # Un poids ?
+
+    def get_new_position_shoulder_right(self, position_to_add: (float, float)):
+        """Move shoulder
+        - position_to_add = (tilt arms to the sides, arms forward / backward)
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 6)[0]
+        return 6, (join_state[0] + position_to_add[0],  # Bras vers l'exterieur
+                   join_state[1],  # rotation de l'epaule -> inutile
+                   join_state[2] + position_to_add[1],  # Bras vers l'avant / arrière
+                   join_state[3])  # Un poids ?
+
+    def get_new_position_shoulder_left(self, position_to_add: (float, float)):
+        """Move shoulder
+        - position_to_add = (tilt arms to the sides, arms forward / backward)
+        """
+        join_state = self.client.getJointStateMultiDof(self.body, 12)[0]
+        return 12, (join_state[0] + position_to_add[0],  # Bras vers l'exterieur
+                    join_state[1],  # Rotation de l'epaule -> inutile
+                    join_state[2] + position_to_add[1],  # Bras vers l'avant / arrière
+                    join_state[3])  # Un poids ?
 
     def apply_motor_power(self):
         # forces = [0.] * len(self.motors_revolute)
@@ -81,24 +194,40 @@ class HumanBody(object):
         #     forces[m] = self.motor_power_revolute[m] * ac * 0.082
         # self.client.setJointMotorControlArray(self.body, self.motors_revolute, controlMode=self.client.TORQUE_CONTROL,
         #                                       forces=forces)
-        forces = [0.] * len(self.motors_spherical)
-        for m in range(len(self.motors_spherical)):
-            limit = 15
-            ac = np.clip(10, -limit, limit)
-            forces[m] = self.motor_power_spherical[m] * ac * 0.082
-        print(forces, self.motors_spherical)
-        kps = [100] * 2
-        kds = [100] * 2
-        joinState = self.client.getJointStateMultiDof(self.body, 6)[0]
-        joinState2 = self.client.getJointStateMultiDof(self.body, 12)[0]
-        print(joinState)
-        # (Vers l'exterieur, rotation de l'epaule, vers l'avant, un poid ?)
-        joinState = (joinState[0] - 0.0001, joinState[1], joinState[2] + 0.0001, joinState[3])
-        joinState2 = (joinState2[0] + 0.0001, joinState2[1], joinState2[2] + 0.0001, joinState2[3])
-        self.client.setJointMotorControlMultiDofArray(self.body, [6, 12],
-                                                      targetPositions=[joinState, joinState2],
-                                                      targetVelocities=[[0, 0, 0], [0, 0, 0]],
+
+        # forces = [0.] * len(self.motors_spherical)
+        # for m in range(len(self.motors_spherical)):
+        #     limit = 15
+        #     ac = np.clip(10, -limit, limit)
+        #     forces[m] = self.motor_power_spherical[m] * ac * 0.082
+
+        moves = [
+            # self.get_new_position_chest((0.0001, 0.0001, 0.0001)),
+            # self.get_new_position_neck(0.0001),
+            # self.get_new_position_hip_right((0.0001, 0.0001)),
+            # self.get_new_position_hip_left((0.0001, 0.0001)),
+            # self.get_new_position_ankle_right(0.0001),
+            # self.get_new_position_ankle_left(0.0001),
+            # self.get_new_position_shoulder_right((-0.0001, 0.0001)),
+            self.get_new_position_shoulder_left((0.0001, 0.0001))
+        ]
+        join_indices = [indices[0] for indices in moves]
+        target_positions = [position[1] for position in moves]
+        target_velocities = [[0, 0, 0]] * len(target_positions)
+        kps = [1000] * len(target_positions)
+        kds = [1000] * len(target_positions)
+        forces = [[1000, 1000, 1000]] * len(target_positions)
+        print(self.body, join_indices,
+              target_positions,
+              target_velocities,
+              self.client.POSITION_CONTROL,
+              kps,
+              kds,
+              forces)
+        self.client.setJointMotorControlMultiDofArray(self.body, join_indices,
+                                                      targetPositions=target_positions,
+                                                      targetVelocities=target_velocities,
                                                       controlMode=self.client.POSITION_CONTROL,
                                                       positionGains=kps,
                                                       velocityGains=kds,
-                                                      forces=[[1000, 1000, 1000], [1000, 1000, 1000]])
+                                                      forces=forces)
